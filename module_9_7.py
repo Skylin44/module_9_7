d7995a23738443827bc8b61f4ea0c0d7b1f9db59
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-11-19 17:48:14 +0300

    "Декораторы"

---
+++ module_9_7.py
@@ -0,0 +1,16 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        if result % 2 == 0:
+            print("Составное")
+        else:
+            print("Простое")
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
